e2c4092140058ec8fc516a55169fd649f3efc732
nehawork <neharamchandani.work@gmail.com>
2022-01-15 13:39:25 +0530

    test

--- packages/web-app/src/index.js
+++ packages/web-app/src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<App />} exact />
         <Route path="/monorepo-react-app" element={<App />} exact />
